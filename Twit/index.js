cf387103b531e0f29fc9866a16fad89919873424
realshak7781 <124501803+realshak7781@users.noreply.github.com>
2024-03-28 18:09:14 +0530

    Index.js created

---
+++ Twit/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+  
