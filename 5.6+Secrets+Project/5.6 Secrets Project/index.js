42c1a9b6877594c428b5a41f634b80b9e6992a49
realshak7781 <124501803+realshak7781@users.noreply.github.com>
2024-04-07 01:03:30 +0530

    Secrets api projecct

---
+++ 5.6+Secrets+Project/5.6 Secrets Project/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import axios from "axios";
+
+const app=express();
+const port=3000;
+
+app.use(express.static("public"));
+
+
+app.get("/",async(req,res)=>{
+   try {
+    const response = await axios.get('https://secrets-api.appbrewery.com/random');
+    res.render("index.ejs",{
+        secret:response.data.secret,
+        user:response.data.username
+    });
+   } 
+   catch (error) {
+    console.log(error.response.data);
+    res.status(500);
+   }
+});
+
+
+app.listen(port,()=>{
+    console.log(`listening on the port ${port}`);
+});
+
+
+
+
+
