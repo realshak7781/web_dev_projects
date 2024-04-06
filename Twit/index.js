adb6bb4c59e692dc1c9cdf09ca171380fe37ed3d
realshak7781 <124501803+realshak7781@users.noreply.github.com>
2024-04-07 02:58:00 +0530

    js change

--- Twit/index.js
+++ Twit/index.js
@@ -1,69 +1,124 @@
-import express from "express";
-import axios from "axios";
-import bodyParser from "body-parser";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { TwitterApi } from 'twitter-api-v2';
+import got from 'got';
+import crypto from 'crypto';
+import OAuth from 'oauth-1.0a';
+import { parse } from 'querystring';
+import { createInterface } from 'readline';
 
-
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-const app = express();
-const port = 3000;
-
-// Serve static files from the 'public' directory
-app.use(express.static(__dirname + '/public'));
-
-// Parse URL-encoded bodies (as sent by HTML forms)
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+const readline = createInterface({
+  input: process.stdin,
+  output: process.stdout
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+const consumer_key = process.env.CONSUMER_KEY;
+const consumer_secret = process.env.CONSUMER_SECRET;
+
+const data = {
+  "text": "Hello world!"
+};
+
+const endpointURL = `https://api.twitter.com/2/tweets`;
+
+const requestTokenURL = 'https://api.twitter.com/oauth/request_token?oauth_callback=oob&x_auth_access_type=write';
+const authorizeURL = new URL('https://api.twitter.com/oauth/authorize');
+const accessTokenURL = 'https://api.twitter.com/oauth/access_token';
+
+const oauth = OAuth({
+  consumer: {
+    key: consumer_key,
+    secret: consumer_secret
+  },
+  signature_method: 'HMAC-SHA1',
+  hash_function: (baseString, key) => crypto.createHmac('sha1', key).update(baseString).digest('base64')
 });
 
-
-
-
-
-async function postTweet() {
-    try {
-        const twitterClient = new TwitterApi({
-            appKey: 'ephNtAW7WFOLHsERn8o1At2TV',
-            appSecret: 'T19hd1MQOmM3Wj4PejoBvhWPfS5TbRcL1ws2hnBj316VLJhLBM',
-            accessToken: '1526942432276914177-Z6F5kOqGEFSbqLSMonszJuFj0aH15O',
-            accessSecret: '1AyKwFVnilqPVe1cb1zx2vb15T9RKQ1zCJLGMuLNEIKrk',
-        });
-
-        const postTweet = await twitterClient.tweets.createTweet({
-            // The text of the Tweet
-            text: "Are you excited for the weekend?",
-
-            // Options for a Tweet with a poll
-            poll: {
-                options: ["Yes", "Maybe", "No"],
-                duration_minutes: 120,
-            },
-        });
-        console.dir(postTweet.data, {
-            depth: null,
-        });
-    } catch (error) {
-        console.log(error);
-    }
+async function input(prompt) {
+  return new Promise(async (resolve, reject) => {
+    readline.question(prompt, (out) => {
+      readline.close();
+      resolve(out);
+    });
+  });
 }
 
-// Route for posting a tweet
-app.post('/tweet', async (req, res) => {
-    try {
-        await postTweet();
-        res.status(200).json({ success: true, message: 'Tweet posted successfully' });
-    } catch (error) {
-        res.status(500).json({ success: false, error: error.message });
+async function requestToken() {
+  const authHeader = oauth.toHeader(oauth.authorize({
+    url: requestTokenURL,
+    method: 'POST'
+  }));
+
+  const req = await got.post(requestTokenURL, {
+    headers: {
+      Authorization: authHeader["Authorization"]
     }
-});
+  });
+  if (req.body) {
+    return parse(req.body);
+  } else {
+    throw new Error('Cannot get an OAuth request token');
+  }
+}
+
+async function accessToken({ oauth_token, oauth_token_secret }, verifier) {
+  const authHeader = oauth.toHeader(oauth.authorize({
+    url: accessTokenURL,
+    method: 'POST'
+  }));
+  const path = `https://api.twitter.com/oauth/access_token?oauth_verifier=${verifier}&oauth_token=${oauth_token}`
+  const req = await got.post(path, {
+    headers: {
+      Authorization: authHeader["Authorization"]
+    }
+  });
+  if (req.body) {
+    return parse(req.body);
+  } else {
+    throw new Error('Cannot get an OAuth request token');
+  }
+}
+
+async function getRequest({ oauth_token, oauth_token_secret }) {
+  const token = {
+    key: oauth_token,
+    secret: oauth_token_secret
+  };
+
+  const authHeader = oauth.toHeader(oauth.authorize({
+    url: endpointURL,
+    method: 'POST'
+  }, token));
+
+  const req = await got.post(endpointURL, {
+    json: data,
+    responseType: 'json',
+    headers: {
+      Authorization: authHeader["Authorization"],
+      'user-agent': "v2CreateTweetJS",
+      'content-type': "application/json",
+      'accept': "application/json"
+    }
+  });
+  if (req.body) {
+    return req.body;
+  } else {
+    throw new Error('Unsuccessful request');
+  }
+}
+
+(async () => {
+  try {
+    const oAuthRequestToken = await requestToken();
+    authorizeURL.searchParams.append('oauth_token', oAuthRequestToken.oauth_token);
+    console.log('Please go here and authorize:', authorizeURL.href);
+    const pin = await input('Paste the PIN here: ');
+    const oAuthAccessToken = await accessToken(oAuthRequestToken, pin.trim());
+    const response = await getRequest(oAuthAccessToken);
+    console.dir(response, {
+      depth: null
+    });
+  } catch (e) {
+    console.log(e);
+    process.exit(-1);
+  }
+  process.exit();
+})();
 
